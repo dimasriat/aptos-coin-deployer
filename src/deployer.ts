faf03f697fd6e2c05f3e9d7e817e29acabbb9e34
Dimas <dimasriat@gmail.com>
2024-08-19 19:38:02 +0700

    feat: tf

--- src/deployer.ts
+++ src/deployer.ts
@@ -1,7 +1,16 @@
-import { Ed25519Account } from "@aptos-labs/ts-sdk";
+import {
+  Account,
+  Aptos,
+  AptosConfig,
+  Ed25519Account,
+  Ed25519PrivateKey,
+  Network,
+} from "@aptos-labs/ts-sdk";
+
 import { exec } from "child_process";
 
-function execPromise(command: string): Promise<void> {
+function execPromise(commandList: string[]): Promise<void> {
+  const command = commandList.join(" \\\n ");
   console.log("executing:\n", command);
   return new Promise((resolve, reject) => {
     exec(command, (err, stdout, stderr) => {
@@ -24,10 +33,15 @@ type ModuleAccount = {
   privateKey: string;
 };
 
-function generateModuleAccount(): ModuleAccount {
+async function generateModuleAccount(): Promise<ModuleAccount> {
   const moduleAccount = Ed25519Account.generate();
   const address = moduleAccount.accountAddress.toString();
   const privateKey = moduleAccount.privateKey.toString();
+
+  await aptos.fundAccount({
+    accountAddress: moduleAccount.accountAddress,
+    amount: 100_000_000,
+  });
 
   const auth: ModuleAccount = { address, privateKey };
 
@@ -37,19 +51,93 @@ function generateModuleAccount(): ModuleAccount {
 }
 
 async function initializeModuleAccount(privateKey: string) {
-  const initCommand = `aptos init \\\n --network devnet --private-key ${privateKey} --assume-yes \n`;
-  await execPromise(initCommand);
+  await execPromise([
+    "aptos init",
+    `--network devnet --private-key ${privateKey} --assume-yes`,
+  ]);
 }
 
 async function publishModule(address: string) {
-  const publishCommand = `aptos move publish \\\n --package-dir src/move --named-addresses MyApp=${address} \\\n --skip-fetch-latest-git-deps --assume-yes \n`;
-  await execPromise(publishCommand);
+  await execPromise([
+    "aptos move publish --package-dir src/move",
+    "--skip-fetch-latest-git-deps --assume-yes",
+    `--named-addresses MyApp=${address}`,
+  ]);
+}
+
+const config = new AptosConfig({ network: Network.DEVNET });
+const aptos = new Aptos(config);
+
+async function transferCoin(
+  moduleAccountPrivateKey: string,
+  receiverAddress: string,
+  amount: number,
+) {
+  const signer = Account.fromPrivateKey({
+    privateKey: new Ed25519PrivateKey(moduleAccountPrivateKey),
+  });
+
+  const txn = await aptos.transaction.build.simple({
+    sender: signer.accountAddress,
+    data: {
+      function: `${signer.accountAddress}::my_coin::transfer`,
+      functionArguments: [receiverAddress, amount],
+    },
+  });
+
+  const commitedTxn = await aptos.signAndSubmitTransaction({
+    signer: signer,
+    transaction: txn,
+  });
+
+  const executedTransaction = await aptos.waitForTransaction({
+    transactionHash: commitedTxn.hash,
+  });
+
+  console.log(JSON.stringify(executedTransaction, null, 2));
+  console.log("Transaction executed:", executedTransaction.hash);
+}
+
+async function registerCoin(
+  privateKey: string,
+  coinTypeAddress: string,
+) {
+  const signer = Account.fromPrivateKey({
+    privateKey: new Ed25519PrivateKey(privateKey),
+  });
+
+  const txn = await aptos.transaction.build.simple({
+    sender: signer.accountAddress,
+    data: {
+      function: `${coinTypeAddress}::my_coin::register`,
+      functionArguments: [],
+    },
+  });
+
+  const commitedTxn = await aptos.signAndSubmitTransaction({
+    signer: signer,
+    transaction: txn,
+  });
+
+  const executedTransaction = await aptos.waitForTransaction({
+    transactionHash: commitedTxn.hash,
+  });
+
+  console.log(JSON.stringify(executedTransaction, null, 2));
+  console.log("Transaction executed:", executedTransaction.hash);
 }
 
 async function main() {
-  const { address, privateKey } = generateModuleAccount();
+  const { address, privateKey } = await generateModuleAccount();
+  const { address: receiverAddress, privateKey: receiverPrivateKey } = await generateModuleAccount();
   await initializeModuleAccount(privateKey);
   await publishModule(address);
+
+  await registerCoin(receiverPrivateKey, address);
+
+  await transferCoin(privateKey, receiverAddress, 10_000_000);
+
+  console.log({ moduleAddress: address, receiverAddress });
 }
 
 main();
--- src/move/sources/my_coin.move
+++ src/move/sources/my_coin.move
@@ -1,7 +1,31 @@
 module MyApp::my_coin {
+    use aptos_framework::coin;
     use std::signer;
+    use aptos_std::event;
 
-    struct MoonCoin {}
+    struct MoonCoin has key {}
+
+    struct TransferOwnershipEvent has drop, store {
+        old_owner: address,
+        new_owner: address
+    }
+
+    struct TransferEvent has drop, store {
+        from: address,
+        to: address,
+        amount: u64
+    }
+
+    struct MoonCoinInfo has key {
+        mint: coin::MintCapability<MoonCoin>,
+        freeze: coin::FreezeCapability<MoonCoin>,
+        burn: coin::BurnCapability<MoonCoin>,
+        owner: address,
+        transfer_ownership_event: event::EventHandle<TransferOwnershipEvent>,
+        transfer_event: event::EventHandle<TransferEvent>,
+    }
+
+    const MOON_COIN_TOKEN: address = @MyApp;
 
     fun init_module(sender: &signer) {
         aptos_framework::managed_coin::initialize<MoonCoin>(
@@ -15,6 +39,33 @@ module MyApp::my_coin {
         aptos_framework::managed_coin::register<MoonCoin>(sender);
 
         let init_supply = 1000000000;
+
+        if (!coin::is_account_registered<MoonCoin>(signer::address_of(sender))) {
+            coin::register<MoonCoin>(sender);
+        };
+
         aptos_framework::managed_coin::mint<MoonCoin>(sender, signer::address_of(sender), init_supply);
     }
+
+    public entry fun register(sender: &signer) {
+        if (!coin::is_account_registered<MoonCoin>(signer::address_of(sender))) {
+            coin::register<MoonCoin>(sender);
+        };
+    }
+
+    public entry fun transfer(sender: &signer, to: address, amount: u64) {
+        // let from = signer::address_of(sender);
+        coin::transfer<MoonCoin>(sender, to, amount);
+        // let moon_coin_info = borrow_global_mut<MoonCoinInfo>(MOON_COIN_TOKEN);
+        // event::emit_event<TransferEvent>(
+        //     &mut moon_coin_info.transfer_event,
+        //     TransferEvent {
+        //         from,
+        //         to,
+        //         amount
+        //     }
+        // );
+    }
+
 }
+
